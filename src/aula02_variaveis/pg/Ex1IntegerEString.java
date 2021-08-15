2d97bfb16ca29f0be112f0b9770e88c93e8350e1
hakacarol <haka.carolina@gmail.com>
2021-08-15 18:46:41 -0300

    adicionando exercicios

--- src/aula02_variaveis/pg/Ex1IntegerEString.java
+++ src/aula02_variaveis/pg/Ex1IntegerEString.java
@@ -4,6 +4,36 @@ Ex02 - Defina duas variáveis Integer e determine pelo valor delas qual é maior
 
 public class Ex1IntegerEString {
     public static void main(String[] args) {
+        // Ex01
+        String frase1 = "Tenho uma banana";
+        String frase2 = "Tenho uma banana e um chiclete";
+        String frase3 = "Tenho uma banana";
 
+        if (frase1.equals(frase2))
+            System.out.println("A frases são iguais");
+        else {
+            System.out.println("As frases são diferentes");
+        }
+        // Testando com a opção de frases iguais
+        if (frase1.equals(frase3))
+            System.out.println("As frases são iguais");
+        else {
+            System.out.println("As frases são diferentes");
+        }
+
+        // Ex02
+        Integer valor1 = 5;
+        Integer valor2 = 15;
+        int comparar;
+
+        if (valor1.equals(valor2))
+            System.out.println("Os dois numeros são iguais a " + valor1);
+        else
+        {comparar = valor1.compareTo(valor2); // valor1 > -> 1 / valor2 > -1
+            if (comparar > 0)
+                System.out.println(valor1 + " é maior que " + valor2);
+            else
+                System.out.println(valor2 + " é maior que " + valor1);
+        }
     }
 }
---
+++ src/aula02_variaveis/pg/ExemploScanner.java
@@ -0,0 +1,4 @@
+package aula02_variaveis.pg;
+
+public class ExemploScanner {
+}
